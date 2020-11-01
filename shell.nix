9a6f39944ea9812a0ba64e23bb318e3aeb7cd7f1
leo60228 <leo@60228.dev>
2020-11-01 14:26:58 -0500

    rewrite shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {} }: with pkgs;
 
-with pkgs; mkShell {
-  buildInputs = [ qt514.qtbase qt514.qtdeclarative ];
+let qtOutput = x: if builtins.elem "bin" x.outputs then x.bin else x.out;
+    makeQtPluginPath = lib.concatMapStringsSep ":" (x: "${qtOutput x}/${qt514.qtbase.qtPluginPrefix}");
+    makeQmlImportPath = lib.concatMapStringsSep ":" (x: "${qtOutput x}/${qt514.qtbase.qtQmlPrefix}");
+in mkShell {
+  buildInputs = [ qt514.qtbase qt514.qtdeclarative kdeFrameworks.qqc2-desktop-style ];
+  QT_PLUGIN_PATH = makeQtPluginPath [ qt514.qtbase qt514.qtquickcontrols2 kdeFrameworks.qqc2-desktop-style ];
+  QML2_IMPORT_PATH = makeQmlImportPath [ qt514.qtdeclarative qt514.qtquickcontrols qt514.qtquickcontrols2 kdeFrameworks.qqc2-desktop-style ];
 }
